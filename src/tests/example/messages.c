cf143046c6be80edfad7e804e71dd0b74af846b9
Chad Versace <chad.versace@intel.com>
2015-05-27 15:00:08 -0700

    tests/example: Add example test for t_failf()

--- src/tests/example/messages.c
+++ src/tests/example/messages.c
@@ -47,3 +47,15 @@ cru_define_test {
     .start = test_assertf,
     .no_image = true,
 };
+
+static void
+test_failf(void)
+{
+    t_failf("i failed on %s", __DATE__);
+}
+
+cru_define_test {
+    .name = "example.messages.failf",
+    .start = test_failf,
+    .no_image = true,
+};
