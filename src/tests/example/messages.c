7d4b63f335d3efff03648dc3d60539c7e50e2214
Chad Versace <chad.versace@intel.com>
2015-05-27 14:18:58 -0700

    tests/example: Add examples for t_assert() variants

---
+++ src/tests/example/messages.c
@@ -0,0 +1,49 @@
+// Copyright 2015 Intel Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice (including the next
+// paragraph) shall be included in all copies or substantial portions of the
+// Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#include <crucible/cru.h>
+
+static void
+test_assert(void)
+{
+    t_assert(2 + 2 == 5);
+}
+
+cru_define_test {
+    .name = "example.messages.assert",
+    .start = test_assert,
+    .no_image = true,
+};
+
+static void
+test_assertf(void)
+{
+    for (int i = 0; i < 8; ++i) {
+        t_assertf(i == -i, "%d is not its additive inverse today (%s)",
+                  i, __DATE__);
+    }
+}
+
+cru_define_test {
+    .name = "example.messages.assertf",
+    .start = test_assertf,
+    .no_image = true,
+};
