aff598bdc38140cb88dd56c9ab48bf42aaa8d965
Chad Versace <chad.versace@intel.com>
2015-08-06 07:59:22 -0700

    cru: Remove accidental header src/crucible/cru_macros.h
    I never accidentally committed it long ago.

--- src/crucible/cru_macros.h
+++
@@ -1,7 +0,0 @@
-// XXX private
-#define cru_constructor __attribute__((constructor))
-
-// XXX private
-#define likely(x) __builtin_expect(!!(x), true)
-#define unlikely(x) __builtin_expect(!!(x), false)
-
