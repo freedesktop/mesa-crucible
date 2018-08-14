9ea5fa0bd9db72f5abb08f35eefc6c359148b579
Jordan Justen <jordan.l.justen@intel.com>
2018-08-14 14:04:08 -0700

    framework: Only get queue family properties once
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/framework/test/t_phase_setup.c
+++ src/framework/test/t_phase_setup.c
@@ -434,12 +434,11 @@ t_setup_vulkan(void)
     uint32_t queue_family_count;
     vkGetPhysicalDeviceQueueFamilyProperties(t->vk.physical_dev,
                                              &queue_family_count, NULL);
-    for (uint32_t i = 0; i < queue_family_count; i++) {
-       VkQueueFamilyProperties *props = malloc(queue_family_count * sizeof(VkQueueFamilyProperties));
-       vkGetPhysicalDeviceQueueFamilyProperties(t->vk.physical_dev,
-                                                &queue_family_count, props);
-       free(props);
-    }
+    VkQueueFamilyProperties *props = malloc(queue_family_count *
+                                            sizeof(VkQueueFamilyProperties));
+    vkGetPhysicalDeviceQueueFamilyProperties(t->vk.physical_dev,
+                                             &queue_family_count, props);
+    free(props);
     qoGetPhysicalDeviceMemoryProperties(t->vk.physical_dev,
                                         &t->vk.physical_dev_mem_props);
 
