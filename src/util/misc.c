48c50767705d66e9b09918d44f32e7fb00303e7e
Jordan Justen <jordan.l.justen@intel.com>
2021-02-15 10:22:58 -0800

    crucible: Define MESON_BUILD_DIR and use in cru_prefix_setup
    With meson builds, the man pages and reference images move under the
    build directory, so we need to account for this in cru_prefix_setup.
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Acked-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/util/misc.c
+++ src/util/misc.c
@@ -80,7 +80,14 @@ static void
 cru_prefix_setup(void)
 {
     static char buf[2048];
+#ifdef MESON_BUILD
+    /* When MESON_BUILD_DIR is defined, then the crucible executable
+     * may be built in a directory named something besides bin.
+     */
+    const char *tail = "/crucible";
+#else
     const char *tail = "/bin/crucible";
+#endif
 
     ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf));
     if (len <= 0) {
@@ -99,7 +106,14 @@ cru_prefix_setup(void)
 
     // Transform PREFIX/bin/crucible to PREFIX.
     path_dirname(&cru_prefix_path_);
+#ifndef MESON_BUILD
+    /* With meson builds, the data & doc direcories are built in
+     * sub-directories parallel to the directory where the crucible
+     * executable is built, but for autotools we need to remove the
+     * bin directory as well.
+     */
     path_dirname(&cru_prefix_path_);
+#endif
 }
 
 const string_t *
